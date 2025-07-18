8946ed3a83609e52131f0545f7299aa42e930822
mestreshake <thiagolhartman@gmail.com>
2025-07-18 20:52:14 -0300

    fix: gh pages deploy config

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
 // src/routes.tsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
@@ -7,7 +7,7 @@ import type { JSX } from 'react';
 
 export default function RoutesApp(): JSX.Element {
   return (
-    <BrowserRouter basename="/jarvis-ts">
+    <HashRouter basename="/jarvis-ts">
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route
@@ -19,6 +19,6 @@ export default function RoutesApp(): JSX.Element {
           }
         />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
